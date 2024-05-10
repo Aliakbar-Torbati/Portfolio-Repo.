fd63bac5e6b444fdb95ad4751d259e737519a32d
Aliakbar Torbati <alitorbati1368@gmail.com>
2024-05-10 12:50:05 +0200

    responsive navbar added

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,25 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import "./NavbarStyle.scss";
 import { Link } from "react-router-dom";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 const Navbar = () => {
+  const [clickNav, setClickNav] = useState(false);
+
+  const handleNavbar = () => {
+    setClickNav(!clickNav);
+  };
+
   return (
     <div className="header">
       <Link to={"/"}>Portfolio</Link>
-    <ul className="nav-menue">
+      <ul className={clickNav ? "nav-menue active" : "nav-menue"}>
         <li>
-            <Link to="/">Home</Link>
+          <Link to="/">Home</Link>
         </li>
         <li>
-            <Link to="/projects">Projects</Link>
+          <Link to="/projects">Projects</Link>
         </li>
         <li>
-            <Link to="/about">About</Link>
+          <Link to="/about">About</Link>
         </li>
         <li>
-            <Link to="/contact">Contact</Link>
+          <Link to="/contact">Contact</Link>
         </li>
-    </ul>
+      </ul>
+      <div className="hamburgerIcon">
+        {!clickNav ? (
+          <FaBars onClick={handleNavbar} size={20} style={{ color: "#fff" }} />
+        ) : (
+          <FaTimes onClick={handleNavbar} size={20} style={{ color: "#fff" }} />
+        )}
+      </div>
     </div>
   );
 };
