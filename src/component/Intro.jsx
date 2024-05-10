1723b87ec222d11831bbe94b2a081c01c786b19f
Aliakbar Torbati <alitorbati1368@gmail.com>
2024-05-10 16:29:03 +0200

    intro section added

---
+++ src/component/Intro.jsx
@@ -0,0 +1,27 @@
+import "./IntroStyle.scss";
+import IntroImg from "../assets/intro.jpg";
+import { Link } from "react-router-dom";
+
+const Intro = () => {
+  return (
+    <div className="introContainer">
+      <div className="introClass">
+        <img className="introImg" src={IntroImg} alt="intro Image" />
+      </div>
+      <div className="introContent">
+        <p>I am Ali</p>
+        <h1>Web Developer</h1>
+        <div>
+          <Link className="btn" to={"/projects"}>
+            Projects
+          </Link>
+          <Link className="btn btn-light" to={"/contact"}>
+            Contact
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Intro;
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from '../component/Navbar'
+import Intro from '../component/Intro'
 
 const Homepage = () => {
   return (
     <div>
       <Navbar />
+      <Intro />
     </div>
   )
 }
