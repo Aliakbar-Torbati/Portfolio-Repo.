d9ccd878200f552312691c4cd6ca469c9913f8b9
Aliakbar Torbati <alitorbati1368@gmail.com>
2024-06-05 17:33:14 +0200

    deployed version1

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import Navbar from '../component/Navbar'
 import Intro from '../component/Intro'
-import ProjectsList from '../component/ProjectsList';
 import Footer from '../component/Footer'
 
 const Homepage = () => {
